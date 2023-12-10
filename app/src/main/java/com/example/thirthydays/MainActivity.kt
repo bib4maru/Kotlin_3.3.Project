5f4e5a1fd9fc6beec3a63a699b7bf37cb58a0b11
bib4maru <kirillosanov@yandex.ru>
2023-12-10 19:15:09 +0300

    Finish app

--- app/src/main/java/com/example/thirthydays/MainActivity.kt
+++ app/src/main/java/com/example/thirthydays/MainActivity.kt
@@ -3,13 +3,43 @@ package com.example.thirthydays
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.thirthydays.model.Day
+import com.example.thirthydays.model.days
 import com.example.thirthydays.ui.theme.ThirthyDaysTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +52,97 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    App()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun App(){
+    Scaffold(
+        topBar = {
+            TopBar()
+        }
+    ) { it ->
+        LazyColumn(contentPadding = it) {
+            items(days){ it ->
+                Item(
+                    day = it,
+                    modifier = Modifier.padding(8.dp))
+            }
+        }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Item(day: Day, modifier: Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+    Card (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        onClick = {expanded = !expanded},
+        colors = CardDefaults.cardColors(
+            containerColor = colorResource(R.color.card_color)
+        )
+
+    ) {
+
+        Column (
+            modifier = Modifier
+                .padding(horizontal =  12.dp, vertical = 10.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "Day ${day.day}",
+                fontSize = 16.sp
+            )
+            Text(
+                text = stringResource(day.name),
+                fontSize = 20.sp
+            )
+            Spacer(modifier = Modifier.size(10.dp))
+            Image(
+                painter = painterResource(day.imageResourceId),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(5.dp))
+                    .fillMaxWidth(),
+            )
+            if (expanded){
+                Spacer(modifier = Modifier.size(10.dp))
+                Text(text = stringResource(day.description))
+            }
+        }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(modifier: Modifier = Modifier){
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge,
+                    fontSize = 24.sp
+                )
+            }
+        },
         modifier = modifier
     )
 }
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ThirthyDaysTheme {
-        Greeting("Android")
+        App()
     }
 }
